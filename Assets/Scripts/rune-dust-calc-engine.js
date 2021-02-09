7aa52d6556d4a60766b310f645e932199f2b6ced
V0ltaroo <69749982+Voltaroo@users.noreply.github.com>
2021-02-09 09:50:27 +0100

    x

--- Assets/Scripts/rune-dust-calc-engine.js
+++ Assets/Scripts/rune-dust-calc-engine.js
@@ -1,12 +1,15 @@
 var stringArr = ['Normal','Improved','Magical','Extraordinary','Legendary'];
 
 var requir_rarity_off = [0,3126,8596,17188,28908];
+var requirROff_count = 57818;
 var melt_rarity_off = [1563,4298,8594,14454,21875];
 
 var requir_rarity_deff = [0,2500,6876,13750,23126];
+var requirRDeff_count = 46252;
 var melt_rarity_deff = [1250,3438,6875,11563,17500];
 
 var requir_rarity_oth = [0,250,688,1375,2313];
+var requirROth_count = 4626;
 var melt_rarity_oth = [125,344,688,1156,1750];
 
 var table01 = getID('table-rarity-stats');
@@ -34,8 +37,53 @@ function generateTables(table,array1,array2,array3){
     
 }
 
+var resultNum = 0;
+
+function generateResult(){
+
+    var runeT0 = parseInt(getID('rune0').value);
+    var runeT1 = parseInt(getID('rune1').value);
+    var runeT2 = parseInt(getID('rune2').value);
+    var runeT3 = parseInt(getID('rune3').value);
+    
+    if(runeT0 == NaN) runeT0 = 0;
+    if(runeT1 == NaN) runeT1 = 0;
+    if(runeT2 == NaN) runeT2 = 0;
+    if(runeT3 == null) runeT3 = 0;
+    
+
+    var type = (getID('rune-type-sel').value);
+    switch(type){
+        case 0:
+        {
+            getID('result').innerHTML = 'Enter rune type!';
+            break;
+        }
+        case 1:
+        {
+            result += (runeT0 * requirROff_count) + (runeT1 * (requirROff_count - 3126)) + (runeT2 * (requirROff_count - 11722)) + (runeT3 * (requirROff_count - 28910));
+            result.innerHTML = 'You will need ' + result + ' rune dust for upgrade your runes.';
+            break;
+        }
+        case 2:
+        {
+            result += (runeT0 * requirRDeff_count) + (runeT1 * (requirRDeff_count - 2500)) + (runeT2 * (requirRDeff_count - 7376)) + (runeT3 * (requirRDeff_count - 21126));
+            result.innerHTML = 'You will need ' + result + ' rune dust for upgrade your runes.';
+            break;
+        }
+        default:
+        {
+            console.log('Unknown type!');
+            break;
+        }
+    }
+
+}
+
 window.onload = () => {
 
     generateTables(table01,requir_rarity_off,requir_rarity_deff,requir_rarity_oth);
     generateTables(table02,melt_rarity_off,melt_rarity_deff,melt_rarity_oth);
 }
+
+window.onchange = () => generateResult();
